876a2de4560e8b1856990d6cd21c79eba49aea0c
DevWavez <devwave@outlook.de>
2023-11-02 10:58:54 +0100

    Added TeamSection component and updated BlogSection component styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import BlogSection from "./components/BlogSection";
+import TeamSection from "./components/TeamSection";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <TeamSection />
       <BlogSection />
     </div>
   );
---
+++ src/components/TeamSection.jsx
@@ -0,0 +1,70 @@
+{
+  /* Person Array */
+}
+
+const people = [
+  {
+    name: "John Doe",
+    role: "CEO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?auto=format&fit=crop&q=80&w=1287&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Design Leader",
+    imageUrl:
+      "https://images.unsplash.com/photo-1554151228-14d9def656e4?auto=format&fit=crop&q=60&w=400&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8R2VzaWNodHxlbnwwfDF8MHx8fDA%3D",
+  },
+  {
+    name: "Daniel",
+    role: "Marketing",
+    imageUrl:
+      "https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?auto=format&fit=crop&q=60&w=400&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8R2VzaWNodHxlbnwwfDF8MHx8fDA%3D",
+  },
+];
+
+const TeamSection = () => {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+        <div className="max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Meet our leadership
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae
+            elementum enim vitae ullamcorper suspendisse.
+          </p>
+        </div>
+
+        {/* Fetch data from Person Array */}
+        <ul
+          role="list"
+          className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
+        >
+          {people.map((person) => (
+            <li key={person.name}>
+              <div className="flex items-center gap-6">
+                <img
+                  className="h-18 w-16 rounded-full"
+                  src={person.imageUrl}
+                  alt=""
+                />
+                <div>
+                  <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                    {person.name}
+                  </h3>
+                  <p className="text-sm font-semibold leading-6 text-indigo-600">
+                    {person.role}
+                  </p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TeamSection;
